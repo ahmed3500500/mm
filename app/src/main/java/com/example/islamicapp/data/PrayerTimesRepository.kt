085071ec9d650e5edfd8688b7546575919981872
ahmed3500500 <ahmed3500599@gmail.com>
2026-01-19 04:29:43 -0600

    first commit

--- app/src/main/java/com/example/islamicapp/data/PrayerTimesRepository.kt
+++ app/src/main/java/com/example/islamicapp/data/PrayerTimesRepository.kt
@@ -1,14 +1,20 @@
 package com.example.islamicapp.data
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object PrayerTimesRepository {
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://api.aladhan.com/v1/")
-        .addConverterFactory(MoshiConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
     private val api = retrofit.create(AlAdhanService::class.java)
